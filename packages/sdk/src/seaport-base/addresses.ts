35b982c001dc34ab1d035ee36eab3ac400ecd3ce
laom <mengx@funsun.cn>
2024-03-14 23:32:58 +0800

    feat: add seaport base ConduitController

--- packages/sdk/src/seaport-base/addresses.ts
+++ packages/sdk/src/seaport-base/addresses.ts
@@ -73,6 +73,7 @@ export const ConduitController: ChainIdToAddress = {
   [Network.Apex]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Blast]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.AstarZkevm]: "0x00000000f9490004c11cef243f5400493c00ad63",
+  [Network.ZklinkTestnet]: "0xdccef9ec0879c951a461a3f4ac293aad39ffbad3"
 };
 
 // https://github.com/ProjectOpenSea/seaport/blob/0a8e82ce7262b5ce0e67fa98a2131fd4c47c84e9/contracts/conduit/ConduitController.sol#L493
