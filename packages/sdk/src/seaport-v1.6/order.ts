7047d639bfeb72e017f60a9e97cb0bd186528cfa
George Roman <30772943+georgeroman@users.noreply.github.com>
2024-03-21 12:26:24 +0100

    fix: tweaks

--- packages/sdk/src/seaport-v1.6/order.ts
+++ packages/sdk/src/seaport-v1.6/order.ts
@@ -1,9 +1,10 @@
+import { TypedDataSigner } from "@ethersproject/abstract-signer";
+
 import { Exchange } from "./exchange";
+import * as Addresses from "../seaport-base/addresses";
+import { cosignOrder } from "../seaport-base/helpers";
 import * as Types from "../seaport-base/types";
 import { Order as OrderV15 } from "../seaport-v1.5/order";
-import { TypedDataSigner } from "@ethersproject/abstract-signer";
-import { cosignOrder } from "../seaport-base/helpers";
-import * as Addresses from "../seaport-base/addresses";
 
 export class Order extends OrderV15 {
   constructor(chainId: number, params: Types.OrderComponents) {
