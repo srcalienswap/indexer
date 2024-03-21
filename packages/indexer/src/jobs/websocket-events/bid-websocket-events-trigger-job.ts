9b624c26c0513443562fc5d528d471c10bc1b816
Idan Peleg <ipeleg@hotmail.com>
2024-03-21 11:53:49 -0400

    feat: disable timeout

--- packages/indexer/src/jobs/websocket-events/bid-websocket-events-trigger-job.ts
+++ packages/indexer/src/jobs/websocket-events/bid-websocket-events-trigger-job.ts
@@ -31,7 +31,7 @@ export class BidWebsocketEventsTriggerQueueJob extends AbstractRabbitMqJobHandle
   queueName = "bid-websocket-events-trigger-queue";
   maxRetries = 5;
   concurrency = 10;
-  timeout = 60000;
+  // timeout = 60000;
   backoff = {
     type: "exponential",
     delay: 1000,
