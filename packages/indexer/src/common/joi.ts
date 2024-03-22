1085378d6016cb113f88ad17f00f0e3789bf9f3c
Idan Peleg <ipeleg@hotmail.com>
2024-03-22 16:21:56 -0400

    feat: added log

--- packages/indexer/src/common/joi.ts
+++ packages/indexer/src/common/joi.ts
@@ -20,6 +20,7 @@ import {
   getUSDAndNativePrices,
   isWhitelistedCurrency,
 } from "@/utils/prices";
+import { logger } from "@/common/logger";
 
 // --- Prices ---
 
@@ -96,6 +97,15 @@ export const getJoiAmountObject = async (
     if (nativeAmount) {
       nativeAmount = subFeeWithBps(nativeAmount, totalFeeBps);
     }
+  }
+
+  if (config.chainId === 1 && !amount) {
+    logger.info(
+      "getJoiAmountObject-debug",
+      `currency=${JSON.stringify(
+        currency
+      )}, amount=${amount}, nativeAmount=${nativeAmount}, usdAmount=${usdAmount}, totalFeeBps=${totalFeeBps}`
+    );
   }
 
   return {
