c25d2fb8b6334ae46a8ed7c4c1f75ce98b84c584
Ofir Sudai <42353301+nofir@users.noreply.github.com>
2024-04-03 10:59:58 -0400

    Ofir/platf 3394 enable expired bids deletion from activities index (#8420)
    * feat: reenable logic
    
    * feat: change limit
    
    * feat: added env var
    
    * feat: revert change
    
    * feat: added queue
    
    * feat: fix sort
    
    * feat: increase delay
    
    * feat: debug logic
    
    * feat: remove image version from update logic
    
    * feat: fix log
    
    * feat: fix log
    
    * feat: add delay
    
    * feat: fix log
    
    * feat: reduce limit
    
    * feat: switch to db, add log

--- packages/indexer/src/api/endpoints/execute/get-execute-bid/v5.ts
+++ packages/indexer/src/api/endpoints/execute/get-execute-bid/v5.ts
@@ -542,9 +542,9 @@ export const getExecuteBidV5Options: RouteOptions = {
           try {
             if (p.token || p.collection) {
               const contract = p.token ? p.token.split(":")[0] : p.collection!;
+              const configV1 = await erc721c.v1.getConfigFromDb(contract);
+              const configV2 = await erc721c.v2.getConfigFromDb(contract);
 
-              const configV1 = await erc721c.v1.getConfig(contract);
-              const configV2 = await erc721c.v2.getConfig(contract);
               if (
                 (configV1 && [4, 6].includes(configV1.transferSecurityLevel)) ||
                 (configV2 && [6, 8].includes(configV2.transferSecurityLevel))
