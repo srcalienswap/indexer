a5c0f206c696ebf17a6a9e78881b430f2f29464c
laom <zhanzhazha001@gmail.com>
2024-08-22 23:22:12 +0800

    feat: format archive s3 bucket name

--- packages/indexer/src/jobs/data-archive/archive-manager.ts
+++ packages/indexer/src/jobs/data-archive/archive-manager.ts
@@ -52,7 +52,9 @@ export class ArchiveManager {
     }
 
     if (nextBatchTime) {
-      const s3Bucket = `${config.environment}-unuevenlabs-database-backup-${getNetworkName()}-alienswap`;
+      const s3Bucket = `${
+        config.environment
+      }-unuevenlabs-database-backup-${getNetworkName()}-alienswap`;
 
       const startTimeMinute = Math.trunc(_.toNumber(format(new Date(nextBatchTime), "m")) / 10);
       const startTime = format(new Date(nextBatchTime), `yyyy-MM-dd HH:${startTimeMinute}0:00`);
